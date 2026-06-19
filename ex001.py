1e63421e624d1b432391ddd559f118a501688512
pantojagabriela131-web <pantojagabriela131@gmail.com>
2026-06-19 11:06:24 -0300

    feat(ex001): adicionar exercício 01

---
+++ ex001.py
@@ -0,0 +1,8 @@
+nome = "Gabriela Pantoja"
+idade = 22
+peso = 70
+print(f"Olá, meu nome é {nome}, tenho {idade} anos e peso {peso} kg.")
+nome = input("Qual é o seu nome? ")
+idade = input("Quantos anos você tem? ")
+peso = input("Quanto você pesa")
+print(f"Olá {nome}, você tem {idade} anos e pesa {peso} kg.")
